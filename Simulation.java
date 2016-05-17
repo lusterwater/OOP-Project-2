80ec4a9c85f41eaa1254c0db7d7c5ecc06767159
lusterwater <calvinlu84@gmail.com>
2016-05-17 09:08:55 -0400

    Update Simulator.java

--- Simulation.java
+++ Simulation.java
@@ -4,30 +4,35 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
-
+import java.util.*;
 import javax.swing.Timer;
 import javax.swing.JPanel;
  
 public class Simulation extends JPanel implements ActionListener {
 
-	public static final int FRAME_RATE = 30;
-	public Timer t;
-	public OrangeRobot oBot;
-	//public BlueRobot bBot;
-	public Buttons button;
-	private int[] test = {30, 10};
+	private static final int FRAME_RATE = 30;
+	private Timer t;
+	private OrangeRobot oBot;
+	private BlueRobot bBot;
+	private Buttons button;
+	private Vector<Vector <String>> posOrder = new Vector<Vector <String>>();
+	private Vector<Vector <Integer>> bluePos = new Vector<Vector <Integer>>();
+	private Vector<Vector <Integer>> oranPos = new Vector<Vector <Integer>>();
+	private int[] test = {30, 10, 40};
+	private int[] test2 = {30, 10, 40};
 	private Scanner inFile;
 	private int cases;
-	public int temp = 0;
+	private int temp = 0;
+	private int temp2 = 0;
 	
 
 	//public int[] answerArr = new int[20];
 	
 	public Simulation(){
-		setBackground(Color.WHITE);
+		setBackground(Color.BLACK);
 		t = new Timer(7200/FRAME_RATE,this);
 		oBot = new OrangeRobot();
+		bBot = new BlueRobot();
 		button = new Buttons();
 	}
 	
@@ -36,18 +41,22 @@ public class Simulation extends JPanel implements ActionListener {
 		
 		super.paintComponent(g);
 		button.drawButton(g);
+		
 		g.setColor(oBot.oRobotColor);
 		g.fillRect(oBot.xCord*15, oBot.yCord, 10, 10);
-		int tempOXCord = oBot.xCord*15;
+		g.setColor(bBot.bRobotColor);
+		g.fillRect(bBot.xCord*15, bBot.yCord, 10, 10);
+		
+		if(bBot.buttonPressed()){	
+			g.setColor(Color.WHITE);
+			g.fillRect(bBot.xCord*15, bBot.yCord+10, 10, 10);
+		}
+		
 		if(oBot.buttonPressed()){	
-			g.setColor(Color.BLACK);
-			g.drawRect(tempOXCord, oBot.yCord+10, 10, 10);
+			g.setColor(Color.WHITE);
+			g.fillRect(oBot.xCord*15, oBot.yCord+10, 10, 10);
 		}
 			
-	
-
-
-		
 	}
 	
 	@Override
@@ -55,7 +64,9 @@ public class Simulation extends JPanel implements ActionListener {
 		if(e.getSource() == t){
 			repaint();
 			oNextMove();
+			bNextMove();
 			oBotMove();
+			bBotMove();
 		}
 		
 	}
@@ -63,7 +74,7 @@ public class Simulation extends JPanel implements ActionListener {
 	//This function just help the Orange Robot move on the screen
 	public void oBotMove(){
 		if(oBot.xCord == oBot.xCordNext){
-			oBot.stay();
+			//This part is just here to stop the frame from moving if there is no more moves
 		}
 		else if(oBot.xCord < oBot.xCordNext ){
 			oBot.goRight();
@@ -77,7 +88,7 @@ public class Simulation extends JPanel implements ActionListener {
 	//Checks if there is a another move for the Orange Robot to go
 	public void oNextMove(){
 		if(temp == test.length-1){
-			oBot.stay();
+			//This part just here so to stop some error codes I'm getting with nullpointer exception
 		}else if(temp < test.length){
 				oBot.xCordNext = test[temp];
 				if(oBot.xCord == oBot.xCordNext){
@@ -89,6 +100,90 @@ public class Simulation extends JPanel implements ActionListener {
 
 	}
 	
+	public void bBotMove(){
+		if(bBot.xCord == bBot.xCordNext){
+			//This part is just here to stop the frame from moving if there is no more moves
+		}
+		else if(bBot.xCord < bBot.xCordNext){
+			bBot.goRight();
+		}
+		else if(bBot.xCord > bBot.xCordNext){
+			bBot.goLeft();
+		}
+	}
+	
+	public void bNextMove(){
+		if(temp2 == test2.length-1){
+			
+		}
+		else if(temp2 < test2.length){
+			bBot.xCordNext = test2[temp2];
+			if(bBot.xCord == bBot.xCordNext){
+				bBot.xCord = bBot.xCordNext;
+				temp2++;
+				bBot.xCordNext = test2[temp2];
+			}
+		}
+	}
+	
+	//Stores the value of the GCJ inputs in vectors so we can use them later in our simulation.
+	public void storeInputs(){
+		for(int i = 0; i < cases; i++){
+			int moves = inFile.nextInt();
+			Vector<String> posTemp = new Vector<String>();
+			Vector<Integer> oPosTemp = new Vector<Integer>();
+			Vector<Integer> bPosTemp = new Vector<Integer>();
+			int j = 0;
+			while(j < moves){
+				/*
+				String something = inFile.next();
+				int something2 = inFile.nextInt();*/
+				if(inFile.next().equals("B")){
+					posTemp.add("True");
+					bPosTemp.add(inFile.nextInt());
+				}
+				else{
+					posTemp.add("False");
+					oPosTemp.add(inFile.nextInt());
+				}
+				j++;
+			}
+		
+			posOrder.add(posTemp);
+			bluePos.add(bPosTemp);
+			oranPos.add(oPosTemp);
+		}
+	}
+	
+	//just to test if the inputs were stored correctly
+	public void printInputs(){
+		for(int i = 0; i < cases; i++){
+			int j = 0;
+			int oC = 0;
+			int bC = 0;
+			int pOsize = posOrder.get(i).size();
+			int bPsize = bluePos.get(i).size();
+			int oPsize = oranPos.get(i).size();
+		
+			while(j < pOsize){
+				System.out.print(posOrder.get(i).get(j) + " ");
+				j++;
+			}
+			System.out.println();
+			if(oranPos.get(i).isEmpty()==false){
+			while(oC < oPsize){
+				System.out.print(oranPos.get(i).get(oC) + " ");
+				oC++;
+			}}
+			System.out.println();
+			while(bC < bPsize){
+				System.out.print(bluePos.get(i).get(bC) + " ");
+				bC++;
+			}
+			System.out.println();
+		}
+	}
+	
 	public void openFile(){
 		try{
 			Path file = Paths.get("A-small-practice.in");
@@ -97,7 +192,6 @@ public class Simulation extends JPanel implements ActionListener {
 		catch(Exception e){
 			System.out.println("There was an error opening your file");
 		}
-	
 	}
 	
 	public void getCases(){
@@ -133,6 +227,7 @@ public class Simulation extends JPanel implements ActionListener {
 		}
 	}
 	*/
+	
 	public void closeFile(){
 		inFile.close();
 	}
@@ -140,6 +235,7 @@ public class Simulation extends JPanel implements ActionListener {
 	public int getCase(){
 		return cases;
 	}
+	
 	/*
 	public void printOut(){
 		for(int i = 0; i < 20; i++){

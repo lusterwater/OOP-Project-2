90f1f1ce1e2597ada50c718aec96470811030f34
lusterwater <calvinlu84@gmail.com>
2016-05-24 18:43:40 -0400

    Update Simulator.java

--- Simulation.java
+++ Simulation.java
@@ -15,62 +15,73 @@ public class Simulation extends JPanel implements ActionListener {
 	private OrangeRobot oBot;
 	private BlueRobot bBot;
 	private Buttons button;
-	private Vector<Vector <Boolean>> posOrder = new Vector<Vector <Boolean>>();
-	private Vector<Vector <Integer>> bluePos = new Vector<Vector <Integer>>();
-	private Vector<Vector <Integer>> oranPos = new Vector<Vector <Integer>>();
-	private int[] test = {10, 60, 25};
-	private int[] test2 = {30, 10, 40};
-	private boolean[] test3 = {true, false, false, true, false, true};
-	private Scanner inFile;
-	private int cases;
-	private int temp = 0;
-	private int temp2 = 0;
-	private int temp3 = 0;
+	private int[] bluePosArr = {61,40,19,94};
+	private int[] oranPosArr = {99,3,22,43,26};
+	private int[] posArr = {1,1,0,0,1,0,1,0,0};
+	private int oPosCount;
+	private int bPosCount;
+	private int posCount;
+	private boolean oButIsPressed;
+	private boolean bButIsPressed;
 			
 	public Simulation(){
 		setBackground(Color.BLACK);
-		t = new Timer(3600/FRAME_RATE,this);
+		t = new Timer(7200/FRAME_RATE,this);
 		oBot = new OrangeRobot();
 		bBot = new BlueRobot();
 		button = new Buttons();
+		oButIsPressed = false;
+		bButIsPressed = false;
+		oPosCount = 0;
+		bPosCount = 0;
+		posCount = 0;
 	}
 				
 	public void paintComponent(Graphics g){
 			
 		super.paintComponent(g);
+		
+		//draws the buttons
 		button.drawButton(g);
-			
+		
+		//draws the two robots
 		g.setColor(oBot.oRobotColor);
 		g.fillRect(oBot.xCord*15, oBot.yCord, 10, 10);
 		g.setColor(bBot.bRobotColor);
 		g.fillRect(bBot.xCord*15, bBot.yCord, 10, 10);
-			
-		if(bBot.buttonPressed() && test3[temp3] == true){	
+		
+		//Check if the button can be pressed and if it can be pressed it shows it graphically
+		if(bBot.buttonPressed() && posArr[posCount] == 1){	
 			g.setColor(Color.WHITE);
 			g.fillRect(bBot.xCord*15, bBot.yCord+10, 10, 10);
-			
+			bButIsPressed = true;
+
 		}
 			
-		if(oBot.buttonPressed() && test3[temp3] == false){	
+		if(oBot.buttonPressed() && posArr[posCount] == 0){	
 			g.setColor(Color.WHITE);
 			g.fillRect(oBot.xCord*15, oBot.yCord+10, 10, 10);
-			
+			oButIsPressed = true;
 		}
-				
-		System.out.println(test3[3]);
-		System.out.println(temp3);
+		
 	}
-
+	
+	//This is where all the animation comes in
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource() == t){
-			repaint();
-			oNextMove();
-			bNextMove();
-			oBotMove();
-			bBotMove();
+			tick();
 		}
+	}
+	
+	//checks for the next order for the robots to move
+	public void botNextSeq(){
+		if(posCount >= posArr.length-1){
 			
+		}
+		else if (oButIsPressed == true || bButIsPressed == true){
+			posCount++;
+		}
 	}
 		
 	//Checks the position of the next coordinate of Orange robot and if he should move left or right or stay still
@@ -84,20 +95,25 @@ public class Simulation extends JPanel implements ActionListener {
 		else if(oBot.xCord > oBot.xCordNext ){
 			oBot.goLeft();
 		}
-			
 	}
 		
 	//Checks if there is a another move for the Orange Robot to go
 	public void oNextMove(){
-		if(temp >= test.length-1 && temp3 >= test3.length-1){
-		//This part just here so to stop some error codes I'm getting with nullpointer exception
-		}else if(temp < test.length && test3[temp3] == false){
-				oBot.xCordNext = test[temp];
-				if(oBot.xCord == oBot.xCordNext){
-					oBot.xCord = oBot.xCordNext;
-					temp++;
-					temp3++;
-					oBot.xCordNext = test[temp];
+		if(oranPosArr.length <= 0){
+		
+		}
+		else{
+			if(oPosCount >= oranPosArr.length-1){
+			
+			}
+			else if(oButIsPressed == true){
+				oBot.xCord = oBot.xCordNext;
+				oButIsPressed = false;
+				oPosCount++;
+				oBot.xCordNext = oranPosArr[oPosCount];
+				
+			}else if(oPosCount == 0){
+				oBot.xCordNext = oranPosArr[oPosCount];
 			}
 		}
 	}
@@ -109,106 +125,45 @@ public class Simulation extends JPanel implements ActionListener {
 		}
 		else if(bBot.xCord < bBot.xCordNext){
 			bBot.goRight();
+		
 		}
 		else if(bBot.xCord > bBot.xCordNext){
 			bBot.goLeft();
+			
 		}
 	}
 	
 	//Checks the container if there are any more moves for the Blue Robot to move
 	public void bNextMove(){
-		if(temp2 >= test2.length-1 && temp3 >= test3.length-1){
+		if(bluePosArr.length <= 0){
 			
 		}
-		else if(temp2 < test2.length && test3[temp3] == true){
-			bBot.xCordNext = test2[temp2];
-			if(bBot.xCord == bBot.xCordNext){
+		else{
+			if(bPosCount >= bluePosArr.length-1){
+			
+			}
+			else if(bButIsPressed == true){
 				bBot.xCord = bBot.xCordNext;
-				temp2++;
-				temp3++;
-				bBot.xCordNext = test2[temp2];
+				bButIsPressed = false;
+				bPosCount++;
+				bBot.xCordNext = bluePosArr[bPosCount];
 			}
-		}
-	}
-		
-	//Stores the value of the GCJ inputs in vectors so we can use them later in our simulation.
-	public void storeInputs(){
-		for(int i = 0; i < cases; i++){
-			int moves = inFile.nextInt();
-			Vector<Boolean> posTemp = new Vector<Boolean>();
-			Vector<Integer> oPosTemp = new Vector<Integer>();
-			Vector<Integer> bPosTemp = new Vector<Integer>();
-			int j = 0;
-			while(j < moves){
-				if(inFile.next().equals("B")){
-					posTemp.add(true);
-					bPosTemp.add(inFile.nextInt());
-				}
-				else{
-					posTemp.add(false);
-					oPosTemp.add(inFile.nextInt());
-				}
-				j++;
+			else if(bPosCount == 0){
+				bBot.xCordNext = bluePosArr[bPosCount];
 			}
-			
-			posOrder.add(posTemp);
-			bluePos.add(bPosTemp);
-			oranPos.add(oPosTemp);
-		}
-	}
-		
-	//just to test if the inputs were stored correctly
-	public void printInputs(){
-		for(int i = 0; i < cases; i++){
-			int j = 0;
-			int oC = 0;
-			int bC = 0;
-			int pOsize = posOrder.get(i).size();
-			int bPsize = bluePos.get(i).size();
-			int oPsize = oranPos.get(i).size();
-			
-			while(j < pOsize){
-				System.out.print(posOrder.get(i).get(j) + " ");
-				j++;
-			}
-			System.out.println();
-			if(oranPos.get(i).isEmpty()==false){
-				while(oC < oPsize){
-					System.out.print(oranPos.get(i).get(oC) + " ");
-					oC++;
-				}
-			}
-			System.out.println();
-			while(bC < bPsize){
-				System.out.print(bluePos.get(i).get(bC) + " ");
-				bC++;
-			}
-			System.out.println();
-		}
-	}
-		
-	public void openFile(){
-		try{
-			Path file = Paths.get("A-small-practice.in");
-			inFile = new Scanner(file);
-		}
-		catch(Exception e){
-			System.out.println("There was an error opening your file");
-		}
+		}	
 	}
 	
-	public void getCases(){
-		cases = inFile.nextInt();
+	public void tick(){
+		repaint();
+		botNextSeq();
+		bNextMove();
+		oNextMove();
+		bBotMove();
+		oBotMove();
 	}
-				
-	public void closeFile(){
-		inFile.close();
-	}
-		
-	public int getCase(){
-		return cases;
-	}
-		
+	
+	//starts the animation
 	public void go(){
 		t.start();
 	}
